c51a9e6afbbf2770807c8b02d48ed62e2590a857
Sergey Dmitriev <dmitriev.sergey58@rwb.ru>
2024-12-30 19:07:34 +0300

    step 2

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 import csv
 import os
 
-from telethon.tl.functions.messages import GetDialogsRequest
-from telethon.tl.types import InputPeerEmpty
+from telethon.tl.functions.messages import GetDialogsRequest, GetHistoryRequest
+from telethon.tl.types import InputPeerEmpty, PeerChannel
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
@@ -75,3 +75,37 @@ with open("members.csv","w",encoding='UTF-8') as f:
        writer.writerow([username,name,target_group.title])     
 print('Парсинг участников группы успешно выполнен.')
 
+all_messages = []
+offset_id = 0
+limit = 100
+total_messages = 0
+total_count_limit = 0
+
+while True:
+   history = client(GetHistoryRequest(
+       peer=target_group,
+       offset_id=offset_id,
+       offset_date=None,
+       add_offset=0,
+       limit=limit,
+       max_id=0,
+       min_id=0,
+       hash=0
+   ))
+   if not history.messages:
+       break
+   messages = history.messages
+   for message in messages:
+       all_messages.append(message.message)
+   offset_id = messages[len(messages) - 1].id
+   if total_count_limit != 0 and total_messages >= total_count_limit:
+       break
+
+print("Сохраняем данные в файл...")
+ 
+with open("chats.csv", "w", encoding="UTF-8") as f:
+   writer = csv.writer(f, delimiter=",", lineterminator="\n")
+   writer.writerow(["message"])
+   for message in all_messages:
+       writer.writerow([message])     
+print("Парсинг сообщений группы успешно выполнен.")
