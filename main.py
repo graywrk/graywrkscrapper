7c4954929237297fa56153eb19475790bf4a22f4
Sergey Dmitriev <dmitriev.sergey58@rwb.ru>
2025-01-03 12:59:43 +0300

    add compose and pymongo

--- main.py
+++ main.py
@@ -19,93 +19,105 @@ client = TelegramClient(phone, api_id, api_hash)
 
 client.start()
 
-chats = []
-last_date = None
-size_chats = 200
-groups = []
+def get_all_chats():
+    
+    chats = []
+    last_date = None
+    size_chats = 200
 
-result = client(GetDialogsRequest(
-    offset_date=last_date,
-    offset_id=0,
-    offset_peer=InputPeerEmpty(),
-    limit=size_chats,
-    hash=0
-))
+    result = client(GetDialogsRequest(
+        offset_date=last_date,
+        offset_id=0,
+        offset_peer=InputPeerEmpty(),
+        limit=size_chats,
+        hash=0
+    ))
 
-chats.extend(result.chats)
+    chats.extend(result.chats)
 
-for chat in chats:
+    return chats
+
+def get_all_groups_from_chats(chats):
+
+    groups = []
+
+    for chat in chats:
+        try:
+            if chat.megagroup == True:
+                groups.append(chat)
+        except:
+            continue
+
+    return groups
+
+def get_all_participants(group):
+
+    all_participants = []
+    
     try:
-        if chat.megagroup == True:
-            groups.append(chat)
+        all_participants = client.get_participants(group)
     except:
-        continue
+        pass
 
-print('Выберите номер группы из перечня:')
-i=0
-for g in groups:
-   print(str(i) + '- ' + g.title)
-   i+=1
+    return all_participants
 
-g_index = input("Введите нужную цифру: ")
-target_group=groups[int(g_index)]
+def get_all_messages(group):
+    
+    all_messages = []
+    offset_id = 0
+    limit = 100
+    total_messages = 0
+    total_count_limit = 100
 
-print('Узнаём пользователей...')
+    while True:
+        history = client(GetHistoryRequest(
+            peer=group,
+            offset_id=offset_id,
+            offset_date=None,
+            add_offset=0,
+            limit=limit,
+            max_id=0,
+            min_id=0,
+            hash=0
+        ))
+        if not history.messages:
+            break
+        messages = history.messages
+        for message in messages:
+            all_messages.append(message.message)
+        offset_id = messages[len(messages) - 1].id
+        if total_count_limit != 0 and total_messages >= total_count_limit:
+            break
+
+    return all_messages
+
+
+groups = get_all_groups_from_chats(get_all_chats())
+
 all_participants = []
-all_participants = client.get_participants(target_group)
- 
+
+for group in groups:
+    all_participants.append(get_all_participants(group))
+    
 print('Сохраняем данные в файл...')
 with open("members.csv","w",encoding='UTF-8') as f:
-   writer = csv.writer(f,delimiter=",",lineterminator="\n")
-   writer.writerow(['username','name','group'])
-   for user in all_participants:
-       if user.username:
-           username= user.username
-       else:
-           username= ""
-       if user.first_name:
-           first_name= user.first_name
-       else:
-           first_name= ""
-       if user.last_name:
-           last_name= user.last_name
-       else:
-           last_name= ""
-       name= (first_name + ' ' + last_name).strip()
-       writer.writerow([username,name,target_group.title])     
-print('Парсинг участников группы успешно выполнен.')
-
-all_messages = []
-offset_id = 0
-limit = 100
-total_messages = 0
-total_count_limit = 0
-
-while True:
-   history = client(GetHistoryRequest(
-       peer=target_group,
-       offset_id=offset_id,
-       offset_date=None,
-       add_offset=0,
-       limit=limit,
-       max_id=0,
-       min_id=0,
-       hash=0
-   ))
-   if not history.messages:
-       break
-   messages = history.messages
-   for message in messages:
-       all_messages.append(message.message)
-   offset_id = messages[len(messages) - 1].id
-   if total_count_limit != 0 and total_messages >= total_count_limit:
-       break
-
-print("Сохраняем данные в файл...")
- 
-with open("chats.csv", "w", encoding="UTF-8") as f:
-   writer = csv.writer(f, delimiter=",", lineterminator="\n")
-   writer.writerow(["message"])
-   for message in all_messages:
-       writer.writerow([message])     
-print("Парсинг сообщений группы успешно выполнен.")
+    writer = csv.writer(f,delimiter="|",lineterminator="\n")
+    writer.writerow(['username','name'])
+    for user in all_participants:
+        try:
+            if user.username:
+                username= user.username
+            else:
+                username= ""
+            if user.first_name:
+                first_name= user.first_name
+            else:
+                first_name= ""
+            if user.last_name:
+                last_name= user.last_name
+            else:
+                last_name= ""
+            name= (first_name + ' ' + last_name).strip()
+            writer.writerow([username,name])
+        except:
+            pass
